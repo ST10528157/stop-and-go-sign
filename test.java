93c31060e4536a618a44babb10294fe1486e14dc
ST10528157 <tameemmo730@gmail.com>
2026-02-27 19:46:15 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,37 @@
+/*
+ *
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author lab_services_student
+ */
+//public class test {stop and go)
+   
+
+
+/**
+*
+* @author ST10528157("HELLO SAM")
+*
+*/
+
+
+public static void main(String[] args) {
+
+System.out.println       ( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" );
+System.out.println       ("   *********            xxxxxxxxx   |\n  ");
+System.out.println       ("  *         *          x         x  |\n  ");
+System.out.println       (" *   STOP    *        x    GO    x |\n  ");
+System.out.println       ("  *         *          x         x  |\n  ");
+System.out.println       ("   *********            xxxxxxxxx   |\n  ");
+System.out.println       ("      **                    XX      |\n  ");
+System.out.println       ("      **                    XX      |\n  ");
+System.out.println       ("      **                    XX      |\n  ");
+System.out.println       ("      **                    XX      |\n  ");
+System.out.println       ("   *********            XXXXXXXXXX  |\n  ");
+
+}
